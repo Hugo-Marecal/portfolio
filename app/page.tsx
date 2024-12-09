983b009ba77283d02592131b05f9af5052f42342
h-dev <hugo.marecal@oclock.school>
2024-12-09 19:42:38 +0100

    setting up bento grid section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl">
         <h1>
+          <FloatingNav navItems={navItems} />
           <Hero />
+          <Grid />
         </h1>
       </div>
     </main>
---
+++ data/index.ts
@@ -0,0 +1,72 @@
+export const navItems = [
+  { name: "Accueil", link: "#accueil" },
+  { name: "About", link: "#about" },
+  { name: "Projet", link: "#projet" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      "Je donne la priorité à la collaboration avec les clients, en favorisant une communication ouverte",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Flexible en ce qui concerne les communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "Ma stack tech",
+    description: "J'essaie constamment de m'améliorer",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Passionné de technologie et de développement",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title:
+      "Actuellement en train de construire un gestionnaire d'actif boursier",
+    description: "",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Voulez-vous démarrer un projet ensemble ?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
