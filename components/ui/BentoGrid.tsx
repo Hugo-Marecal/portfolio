2e03e10a322024948312837205a44279511b0f5b
h-dev <hugo.marecal@oclock.school>
2024-12-17 15:36:43 +0100

    update Lottie for better implementation

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -2,11 +2,17 @@
 import { cn } from "@/lib/utils";
 import { BackgroundGradientAnimation } from "./GradientBg";
 import GridGlobe from "./GridGlobe";
-import Lottie from "react-lottie";
 import { useState } from "react";
 import animationData from "@/data/confetti.json";
 import MagicButton from "./MagicButton";
 import { IoCopyOutline } from "react-icons/io5";
+
+import dynamic from "next/dynamic";
+
+const Player = dynamic(
+  () => import("@lottiefiles/react-lottie-player").then((mod) => mod.Player),
+  { ssr: false }
+);
 
 export const BentoGrid = ({
   className,
@@ -47,11 +53,16 @@ export const BentoGridItem = ({
   spareImg?: string;
 }) => {
   const [copied, setCopied] = useState(false);
+  const [showConfetti, setShowConfetti] = useState(false);
 
   const handleCopy = () => {
-    navigator.clipboard.writeText("your@email.com");
+    navigator.clipboard.writeText(process.env.NEXT_PUBLIC_MY_EMAIL || "");
 
     setCopied(true);
+    setShowConfetti(true);
+
+    setTimeout(() => setShowConfetti(false), 3000);
+    setTimeout(() => setCopied(false), 5000);
   };
 
   return (
@@ -107,48 +118,17 @@ export const BentoGridItem = ({
 
           {id === 2 && <GridGlobe />}
 
-          {/* {id === 3 && (
-            <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2">
-              <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
-                {["React.js", "Next.js", "TypeScript"].map((item) => (
-                  <span
-                    key={item}
-                    className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
-                  >
-                    {item}
-                  </span>
-                ))}
-                <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
-              </div>
-              <div className="flex flex-col gap-3 md:gap-3 lg:gap-8">
-                <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]" />
-                {["PostgreSQL", "Node.js", "JAVA"].map((item) => (
-                  <span
-                    key={item}
-                    className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]"
-                  >
-                    {item}
-                  </span>
-                ))}
-              </div>
-            </div>
-          )} */}
-
           {id === 6 && (
             <div className="mt-5 relative">
               <div className={`absolute -bottom-5 right-0`}>
-                <Lottie
-                  options={{
-                    loop: copied,
-                    autoplay: copied,
-                    animationData,
-                    rendererSettings: {
-                      preserveAspectRatio: "xMidYMid slice",
-                    },
-                  }}
-                  height={200}
-                  width={400}
-                />
+                {showConfetti && (
+                  <Player
+                    autoplay
+                    loop={false}
+                    src={animationData}
+                    className="w-[200px] h-[200px] md:w-[300px] xl:w-[400px] md:h-[300px]"
+                  />
+                )}
               </div>
 
               <MagicButton
